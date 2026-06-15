404dc41c6f1e74fe1ff8dc87c60034ba8fc10b07
AbdelRahman Shalan <abdelrahman.shalaan03@gmail.com>
2026-06-15 18:59:43 +0300

    refactor: update app routes and remove unused endpoints

--- lib/core/values/app_routs_name.dart
+++ lib/core/values/app_routs_name.dart
@@ -1,7 +1,7 @@
 abstract class AppRoutsName {
   static const String splashScreen = '/splashScreen';
-  static const String onBoardingScreen = '/onBoardingScreen';
   static const String loginScreen = '/loginScreen';
   static const String registerScreen = '/registerScreen';
   static const String homeScreen = '/homeScreen';
+  static const String forgetPasswordScreen = '/forgetPasswordScreen';
 }
--- lib/core/values/endpoints.dart
+++ lib/core/values/endpoints.dart
@@ -1,7 +1,3 @@
 abstract class Endpoints {
-  static const String baseUrl = 'https://your-api-base-url.com/api/';
 
-  static const String home = '${baseUrl}driver/home';
-  static const String orders = '${baseUrl}driver/orders';
-  static const String profile = '${baseUrl}driver/profile';
 }
