20d753e82164fbd4b4e8eb32f684946c96d4402e
ahmedmohsen087 <ahmedmohsen0871@gmail.com>
2026-06-24 12:24:13 +0300

    start ui

---
+++ lib/features/home/presentation/widgets/flower_order_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FlowerOrderItem extends StatelessWidget {
+  const FlowerOrderItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+
+      ),
+
+
+    );
+  }
+}
